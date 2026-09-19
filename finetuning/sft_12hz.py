e87e9d5b0176fda0e2702ba0b73e4e46113a51e2
Mahmoud Ibrahim <mahmoudibrahimabdelfattah48@gmail.com>
2026-09-19 15:35:46 +0300

    update file and solve tensorboard

--- finetuning/sft_12hz.py
+++ finetuning/sft_12hz.py
@@ -53,7 +53,10 @@ def train():
     parser.add_argument("--speaker_name", type=str, default="speaker_test")
     args = parser.parse_args()
 
-    accelerator = Accelerator(gradient_accumulation_steps=4, mixed_precision="bf16", log_with="tensorboard")
+    # accelerator = Accelerator(gradient_accumulation_steps=4, mixed_precision="bf16", log_with="tensorboard")
+
+    accelerator = Accelerator(gradient_accumulation_steps=4, mixed_precision="bf16")
+
 
     MODEL_PATH = args.init_model_path
 
